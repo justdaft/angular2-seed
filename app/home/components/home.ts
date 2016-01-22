3079099f2f15af38a95d59ad71357aec4d8e3938
billy westbury <billy.westbury@gmail.com>
2016-01-22 12:28:10 +0000

    quick save

--- app/home/components/home.ts
+++ app/home/components/home.ts
@@ -9,35 +9,38 @@ import {Component} from 'angular2/core';
   styleUrls: ['./home/components/home.css']
 })
 export class HomeCmp {
-  game: Phaser.Game;
-
+  game:Phaser.Game;
+  textValue: Phaser.Text;
+  updateCount: number;
   constructor() {
-    this.game = new Phaser.Game(800, 800, Phaser.AUTO, 'content', {
-            create: this.create,
-            preload: this.preload,
-            update: this.update,
-            render: this.render
-        });
+    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
+      create: this.create,
+      preload: this.preload,
+      update: this.update,
+      render: this.render
+    });
   }
 
   render() {
-      this.game.debug.text('This is drawn in render()', 0, 50);
-      return this.render;
-      };
+    this.game.debug.text('This is drawn in render()', 0, 50);
+    return this.render;
+  };
 
   update() {
-      this.textValue.text = (this.updateCount++).toString();
-      return this.update;
-      };
+    this.textValue.text = (this.updateCount++).toString();
+    return this.update;
+  };
 
   preload() {
-      return this.preload;
-      };
+    this.game.load.tilemap('matching', './assets/phaser_tiles.json', null, Phaser.Tilemap.TILED_JSON);
+    this.game.load.image('tiles', './assets/phaser_tiles.png');//, 100, 100, -1, 1, 1);
+    return this.preload;
+  };
 
   create() {
-      let style = { font: '36px Arial', fill: '#808080', align: 'center' };
-      this.textValue = this.game.add.text(0, 0, '0', style);
-      this.updateCount = 0;
-      // return this.create;
-      };
+    let style = {font: '36px Arial', fill: '#808080', align: 'center'};
+    this.textValue = this.game.add.text(0, 0, '0', style);
+    this.updateCount = 0;
+    return this.create;
+  };
 }

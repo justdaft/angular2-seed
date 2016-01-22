c19cb0a28ca6683d6c6665dc962c0264d5b4dbd8
billy westbury <billy.westbury@gmail.com>
2016-01-22 11:44:07 +0000

    new start

--- app/home/components/home.ts
+++ app/home/components/home.ts
@@ -1,3 +1,6 @@
+/// <reference path='../../../node_modules/phaser/typescript/phaser.comments.d.ts' />
+/// <reference path='../../../node_modules/phaser/typescript/pixi.comments.d.ts' />
+
 import {Component} from 'angular2/core';
 
 @Component({
@@ -5,4 +8,14 @@ import {Component} from 'angular2/core';
   templateUrl: './home/components/home.html',
   styleUrls: ['./home/components/home.css']
 })
-export class HomeCmp {}
+export class HomeCmp {
+  game: Phaser.Game;
+  constructor() {
+    this.game = new Phaser.Game(800, 800, Phaser.AUTO, 'content', {
+            create: this.create,
+            preload: this.preload,
+            update: this.update,
+            render: this.render
+        });
+  }
+}
